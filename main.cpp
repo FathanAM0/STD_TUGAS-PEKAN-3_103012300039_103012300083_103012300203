e7019c467fe7a6176ebd2642bf038417a5cc156c
Fathan Arya Maulana <fathana.mrj951@gmail.com>
2024-10-08 20:20:34 +0700

    Update main2.cpp

--- main.cpp
+++ main.cpp
@@ -6,53 +6,51 @@ int main()
 {
     List FHI;
     infotype data1,data2,data3,data4,data5,data6;
-    adr P,prec, loc;
+    adr P1, P2, P3, P4, P5, P6,prec, loc;
 
     Create_List(FHI);
 
     Input_Data(data1);
-    P = New_Elemen(data1);
-    Insert_First(FHI,P);
+    P1 = New_Elemen(data1);
+    Insert_First(FHI,P1);
 
     Input_Data(data2);
-    P = New_Elemen(data2);
-    Insert_Last(FHI,P);
+    P2 = New_Elemen(data2);
+    Insert_Last(FHI,P2);
 
     Input_Data(data3);
-    P = New_Elemen(data3);
-    Insert_First(FHI,P);
+    P3 = New_Elemen(data3);
+    Insert_First(FHI,P3);
 
     Input_Data(data4);
-    P = New_Elemen(data4);
-    Insert_Last(FHI,P);
+    P4 = New_Elemen(data4);
+    Insert_Last(FHI,P4);
 
     Input_Data(data5);
-    P = New_Elemen(data5);
+    P5 = New_Elemen(data5);
     prec = SearchBy_Pemesan(FHI,data4);
 
-    Insert_After(FHI,prec,P);
+    Insert_After(FHI,prec,P5);
 
     Input_Data(data6);
-    P = New_Elemen(data6);
+    P6 = New_Elemen(data6);
     prec = SearchBy_Pemesan(FHI,data4);
 
-    Insert_After(FHI,prec,P);
+    Insert_After(FHI,prec,P6);
     cout << endl << endl;
 
-    cout << "Data Sebelum Di Sorting dan Delete :" << endl;
     Show(FHI);
     cout << endl << endl <<endl;
+    
+    //menghapus data kedua
+    Delete_After(FHI,first(FHI),P2);
 
-    cout << "Data Sesudah Di Sorting dan Delete :" << endl;
-    //delete data kedua
-    Delete_After(FHI,first(FHI),P);
-
-    //delete data keenam
+    //menghapus data keenam
     loc = (SearchBy_Pemesan(FHI,data6));
 
-    prec = Cari_Prec(FHI,loc,P);
+    prec = Cari_Prec(FHI,loc,P5);
 
-    Delete_After(FHI,prec,P);
+    Delete_After(FHI,prec,P5);
 
     SortByHargaTiket(FHI);
 
